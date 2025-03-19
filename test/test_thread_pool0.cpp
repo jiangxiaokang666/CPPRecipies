b6ffba984064fa4d686ba3b02d5f28bbd6701288
XiaokangJiang <385807587@qq.com>
2025-03-19 10:46:10 +0800

    test thread pool early stop

--- test/test_thread_pool0.cpp
+++ test/test_thread_pool0.cpp
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <chrono>
+#include <atomic>
 
 void Print()
 {
@@ -33,13 +34,43 @@ void Test(size_t max_size)
     pool.Run(std::bind(&CountDownLatch::CountDown,&count_down));
     pool.Stop();
 }
+std::atomic_int g_count;
+
+void LongFunc()
+{
+    std::this_thread::sleep_for(std::chrono::milliseconds(300));
+    printf("execute long func %dd %d\n",std::this_thread::get_id(),g_count++);
+}
+
+void Test2()
+{
+    g_count = 0;
+    printf("Test thread pool early stop\n");
+    ThreadPool pool;
+    pool.SetMaxQueueSize(5);
+    pool.Start(5);
+    std::thread thread0([&pool]()
+    {
+        for(size_t  i = 0 ; i<20;++i)
+        {
+            pool.Run(std::bind(LongFunc));
+        }
+    });
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    pool.Stop();
+    thread0.join();
+    pool.Run(Print);
+    printf("Test 2 done.\n");
+}
 
 int main()
 {
-    Test(0);
-    Test(1);
-    Test(5);
-    Test(20);
-    Test(50);
+    // Test(0);
+    // Test(1);
+    // Test(5);
+    // Test(20);
+    // Test(50);
+    Test2();
     return 0;
 }
